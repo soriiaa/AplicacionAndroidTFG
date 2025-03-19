81e1c59eb27a0da3168e9042bc7697bbf678d178
soriiaa <alex.soriaalcaraz@gmail.com>
2025-03-19 09:55:48 +0100

    Commit 2 - A partir de aquí empiezo la pantalla de carga

--- app/src/main/java/com/example/investlearntfg/MainActivity.kt
+++ app/src/main/java/com/example/investlearntfg/MainActivity.kt
@@ -23,18 +23,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     InvestLearnTFGTheme {
-        Greeting("Android")
+
     }
 }
